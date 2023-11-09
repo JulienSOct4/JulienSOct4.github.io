d4a45f46e5e6af7ad0e1e129dd6773ce0b6e9c54
Julien Suarez <jjacobsuarez@gmail.com>
2023-11-09 18:12:37 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
         // TODO 2 : Create a function that draws a circle 
         function drawCircle() {
             circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
             view.addChild(circle);
             circles.push(circle);
         }
@@ -56,12 +56,12 @@ var init = function (window) {
         physikz.updatePosition(circles[4]);
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-        game.checkCirclePosition(circles[0]);
-        game.checkCirclePosition(circles[1]);
-        game.checkCirclePosition(circles[2]);
-        game.checkCirclePosition(circles[3]);
-        game.checkCirclePosition(circles[4]);
-
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
+    
             // TODO 9 : Iterate over the array
            
             
@@ -75,12 +75,11 @@ var init = function (window) {
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
-            
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+           if (circle.x > canvas.width) {
+            circle.x = 0
+           }
+        
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
