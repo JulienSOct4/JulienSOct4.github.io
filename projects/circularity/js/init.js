90c8b8e290b6a2bfe67773725349e5658bf6a5c8
Julien Suarez <jjacobsuarez@gmail.com>
2023-11-15 18:09:11 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -53,23 +53,27 @@ var init = function (window) {
         and check to see if it has drifted off the screen.         
         */
         function update() {
-            // TODO 4 : Update the circle's position //
-        physikz.updatePosition(circles[0]);
-        physikz.updatePosition(circles[1]);
-        physikz.updatePosition(circles[2]);
-        physikz.updatePosition(circles[3]);
-        physikz.updatePosition(circles[4]);
             
+            // TODO 4 : Update the circle's position //
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+
+
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
             game.checkCirclePosition(circles[0]);
             game.checkCirclePosition(circles[1]);
             game.checkCirclePosition(circles[2]);
             game.checkCirclePosition(circles[3]);
             game.checkCirclePosition(circles[4]);
-    
+
+
             // TODO 9 : Iterate over the array
            
             
+         
         }
     
         /* 
@@ -83,11 +87,11 @@ var init = function (window) {
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
            if (circle.x > canvas.width) {
             circle.x = 0
-           } else if (circle.y > canvas.height) {
+           } if (circle.y > canvas.height) {
             circle.y = 0
-           } else if (circle.x < 0) {
+           } if (circle.x < 0) {
             circle.x = canvas.width
-           } else if (circle.y < 0) {
+           } if (circle.y < 0) {
             circle.y = canvas.height
            };
         
